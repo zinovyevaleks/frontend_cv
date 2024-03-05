43b1c1a166a757d19bd45ad2c799e971789969ae
Aleksandr Zinovyev <zinovyevaleks@yandex.ru>
2024-03-05 15:02:36 +0700

    Add Fira code font

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,20 @@ module.exports = {
     title: `frontend_CV`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-postcss"]
+  plugins: [
+    "gatsby-plugin-postcss",
+    {
+      resolve: `gatsby-omni-font-loader`,
+      options: {
+        enableListener: true,
+        preconnect: [`https://fonts.googleapis.com`, `https://fonts.gstatic.com`],
+        web: [
+          {
+            name: `Fira Code`,
+            file: `https://fonts.googleapis.com/css2?family=Fira+Code:wght@300..700&display=swap`,
+          },
+        ],
+      },
+    },
+  ]
 };
